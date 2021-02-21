bcfb9f9c2c17ef3bb22d03c7362db7d5462fdca7
Polina Romankova <pola.royn@tut.by>
2021-02-21 23:13:48 +0300

    f

--- serverWork.js
+++ serverWork.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
     next();
 });
 
-    app.use(cors())
+    // app.use(cors())
 
     app.post('/auth' , (req , res) => {
         sql.checkUser(connection , req.body.name).then((result) => {
