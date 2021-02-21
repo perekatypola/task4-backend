bdabddfffafc72fbfea2733fdb37a74c11daa0bf
Polina Romankova <pola.royn@tut.by>
2021-02-21 23:10:10 +0300

    f

--- serverWork.js
+++ serverWork.js
@@ -12,7 +12,7 @@ const {decodeToken} = require("./safety");
 
 app.use((req, res, next) => {
 
-    res.header("Access-Control-Allow-Origin",  '*');
+    res.header("Access-Control-Allow-Origin",  'https://task4-frontend.herokuapp.com');
 
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 
