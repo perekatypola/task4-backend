a03bac63b69a5d0194d8907074466d57b015014a
Polina Romankova <pola.royn@tut.by>
2021-02-21 11:26:26 +0300

    fix

--- serverWork.js
+++ serverWork.js
@@ -12,7 +12,7 @@ const {decodeToken} = require("./safety");
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", 'https://task4-frontend.herokuapp.com/');
     res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     res.header('Access-Control-Expose-Headers' , 'Access-Control-Allow-Origin')
     next();
